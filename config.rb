388a9a641c6ba895e5845be82d6d0a7bd622b9cf
Leigh McCulloch <leigh@gumroad.com>
2014-07-01 11:26:17 -0700

    added imageoptim

--- config.rb
+++ config.rb
@@ -12,6 +12,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :directory_indexes
+
 configure :build do
   activate :minify_html
   activate :minify_css
@@ -19,6 +21,13 @@ configure :build do
   activate :gzip
   activate :asset_hash, :ignore => [/^images\//]
   activate :relative_assets
+
+  # Requires installing image_optim extensions.
+  # Ref: https://github.com/toy/image_optim
+  # 1) `brew install advancecomp gifsicle jhead jpegoptim jpeg optipng pngcrush`
+  # 2) Download pngout from http://www.jonof.id.au/kenutils
+  # 3) `cp pngout /usr/local/bin/pngout`
+  activate :imageoptim
 end
 
 activate :deploy do |deploy|
